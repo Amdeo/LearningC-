075aeea8f3c72af592dcde59093c33bd817ec08b
Amdeo <774659391@qq.com>
2020-07-05 20:24:58 +0800

    已删除        .vscode/c_cpp_properties.json 已修改        .gitignore Add        process/create.cpp

---
+++ process/create.cpp
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main()
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        cout << "error" << endl;
+        
+    }
+
+    if (pid == 0)
+    {
+        cout << "子进程" << endl;
+    }
+    else
+    {
+        cout << "父进程" << endl;
+    }
+    return 0;
+}
